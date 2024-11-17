24caf6d9190b09489ea6d6aa681b1365b9591986
Karl Håkansson <webmaster@tunkdesign.com>
2024-11-17 15:55:25 +0100

    Prepared system for a dark theme.

--- config/config.php
+++ config/config.php
@@ -1,4 +1,34 @@
 <?php
-// $name = preg_replace("/[^a-z\d]/i", "", __DIR__);
-// session_name($name);
-// session_start();
+// Start session.
+if (session_status() == PHP_SESSION_NONE) {
+  $name = preg_replace("/[^a-z\d]/i", "", __DIR__);
+  session_name($name);
+  session_start();
+}
+
+// If GET action.
+if (isset($_GET["action"])) {
+
+  // Change theme with GET theme.
+  if ($_GET["action"] == "theme") {
+      $previousValue = isset($_SESSION["theme"]) ? $_SESSION["theme"] : null;
+
+      if ($previousValue == "dark") {
+          unset($_SESSION["theme"]);
+      } else {
+          $_SESSION["theme"] = "dark";
+      }
+
+      $url = $_SERVER["REQUEST_SCHEME"] . "://" . $_SERVER["HTTP_HOST"] . $_SERVER["PHP_SELF"];
+      $url = preg_replace("/index.php\//", "", $url);
+      header("Location: $url");
+  }
+
+  // Destroy session with GET action.
+  if ($_GET["action"] == "session_destroy") {
+      session_destroy();
+      $url = $_SERVER["REQUEST_SCHEME"] . "://" . $_SERVER["HTTP_HOST"] . $_SERVER["PHP_SELF"];
+      $url = preg_replace("/index.php\//", "", $url);
+      header("Location: $url");
+  }
+}
--- content/_meta.md
+++ content/_meta.md
@@ -6,4 +6,10 @@ Social:
     - title: Länk till sidans github repo.
       url: https://github.com/TunkDesign/PA1436
       icon: fab fa-github
+    - title: Radera session-data.
+      url: ?action=session_destroy
+      icon: fas fa-trash
+    - title: Ändra tema.
+      url: ?action=theme
+      icon: fas fa-lightbulb
 ---
